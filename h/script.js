bb38bfe0f2799b3ae3b364bea10aa439e6558402
Satyaki SahaE <satyaki95@gmail.com>
2022-09-18 21:01:15 +0530

    u

---
+++ h/script.js
@@ -0,0 +1,109 @@
+// Trainings Start
+const trainings = document.getElementById("trainings");
+const trainings2 = document.getElementsByClassName("trainings")[0];
+trainings.addEventListener('mouseover', () => 
+{trainings2.style.display = 'block'});
+
+trainings.addEventListener('mouseout', () => 
+{trainings2.style.display = 'none'});
+// Trainings end
+
+// Meetups Start
+const meetups = document.getElementById("meetups");
+const meetups2 = document.getElementsByClassName("meetups")[0];
+meetups.addEventListener('mouseover', () => 
+{meetups2.style.display = 'block'});
+
+meetups.addEventListener('mouseout', () => 
+{meetups2.style.display = 'none'});
+// Meetups end
+
+// Webinars Start
+const webinars = document.getElementById("webinars");
+const webinars2 = document.getElementsByClassName("webinars")[0];
+webinars.addEventListener('mouseover', () => 
+{webinars2.style.display = 'block'});
+
+webinars.addEventListener('mouseout', () => 
+{webinars2.style.display = 'none'});
+// Webinars End
+
+// Conference start
+const conference = document.getElementById("conference");
+const conference2 = document.getElementsByClassName("conference")[0];
+conference.addEventListener('mouseover', () => 
+{conference2.style.display = 'block'});
+
+conference.addEventListener('mouseout', () => 
+{conference2.style.display = 'none'});
+// Conference end
+
+// Videos Start
+const videos = document.getElementById("videos");
+const videos2 = document.getElementsByClassName("videos")[0];
+videos.addEventListener('mouseover', () => 
+{videos2.style.display = 'block'});
+
+videos.addEventListener('mouseout', () => 
+{videos2.style.display = 'none'});
+// Videos End
+
+// PM Speak Series Start
+const pm = document.getElementById("pm");
+const pm2 = document.getElementsByClassName("pm")[0];
+pm.addEventListener('mouseover', () => 
+{pm2.style.display = 'block'});
+
+pm.addEventListener('mouseout', () => 
+{pm2.style.display = 'none'});
+// PM Speak Series End
+
+// Blogs Start
+const blogs = document.getElementById("blogs");
+const blogs2 = document.getElementsByClassName("blogs")[0];
+blogs.addEventListener('mouseover', () => 
+{blogs2.style.display = 'block'});
+
+blogs.addEventListener('mouseout', () => 
+{blogs2.style.display = 'none'});
+// Blogs End
+
+// Podcasts Start
+const podcasts = document.getElementById("podcasts");
+const podcasts2 = document.getElementsByClassName("podcasts")[0];
+podcasts.addEventListener('mouseover', () => 
+{podcasts2.style.display = 'block'});
+
+podcasts.addEventListener('mouseout', () => 
+{podcasts2.style.display = 'none'});
+// Podcasts End
+
+// Be A Speaker Start
+const speaker = document.getElementById("speaker");
+const speaker2 = document.getElementsByClassName("speaker")[0];
+speaker.addEventListener('mouseover', () => 
+{speaker2.style.display = 'block'});
+
+speaker.addEventListener('mouseout', () => 
+{speaker2.style.display = 'none'});
+// Be A Speaker End
+
+// Be A Volunteer Start
+const volunteer = document.getElementById("volunteer");
+const volunteer2 = document.getElementsByClassName("volunteer")[0];
+volunteer.addEventListener('mouseover', () => 
+{volunteer2.style.display = 'block'});
+
+volunteer.addEventListener('mouseout', () => 
+{volunteer2.style.display = 'none'});
+// Be A Volunteer End
+
+// About Us Start 
+const about = document.getElementById("about");
+const about2 = document.getElementsByClassName("about")[0];
+about.addEventListener('mouseover', () => 
+{about2.style.display = 'block'});
+
+about.addEventListener('mouseout', () => 
+{about2.style.display = 'none'});
+// About Us End
