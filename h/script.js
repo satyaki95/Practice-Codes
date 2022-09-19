6e5bfe10a1a766bfeb43fdc1b8548d4fef4aad9b
Satyaki SahaE <satyaki95@gmail.com>
2022-09-19 12:16:30 +0530

    u

--- h/script.js
+++ h/script.js
@@ -1,109 +1,175 @@
 // Trainings Start
 const trainings = document.getElementById("trainings");
 const trainings2 = document.getElementsByClassName("trainings")[0];
-trainings.addEventListener('mouseover', () => 
+trainings.addEventListener('mousemove', () => 
 {trainings2.style.display = 'block'});
 
-trainings.addEventListener('mouseout', () => 
+trainings2.addEventListener('mousemove', () =>
+{trainings2.style.display = 'block'});
+
+trainings.addEventListener('mouseleave', () => 
+{trainings2.style.display = 'none'});
+
+trainings2.addEventListener('mouseleave', () =>
 {trainings2.style.display = 'none'});
 // Trainings end
 
 // Meetups Start
 const meetups = document.getElementById("meetups");
 const meetups2 = document.getElementsByClassName("meetups")[0];
-meetups.addEventListener('mouseover', () => 
+meetups.addEventListener('mousemove', () => 
 {meetups2.style.display = 'block'});
 
-meetups.addEventListener('mouseout', () => 
+meetups2.addEventListener('mousemove', () =>
+{meetups2.style.display = 'block'});
+
+meetups.addEventListener('mouseleave', () => 
+{meetups2.style.display = 'none'});
+
+meetups2.addEventListener('mouseleave', () =>
 {meetups2.style.display = 'none'});
 // Meetups end
 
 // Webinars Start
 const webinars = document.getElementById("webinars");
 const webinars2 = document.getElementsByClassName("webinars")[0];
-webinars.addEventListener('mouseover', () => 
+webinars.addEventListener('mousemove', () => 
 {webinars2.style.display = 'block'});
 
-webinars.addEventListener('mouseout', () => 
+webinars2.addEventListener('mousemove', () => 
+{webinars2.style.display = 'block'});
+
+webinars.addEventListener('mouseleave', () => 
+{webinars2.style.display = 'none'});
+
+webinars2.addEventListener('mouseleave', () => 
 {webinars2.style.display = 'none'});
 // Webinars End
 
 // Conference start
 const conference = document.getElementById("conference");
 const conference2 = document.getElementsByClassName("conference")[0];
-conference.addEventListener('mouseover', () => 
+conference.addEventListener('mousemove', () => 
 {conference2.style.display = 'block'});
 
-conference.addEventListener('mouseout', () => 
+conference2.addEventListener('mousemove', () => 
+{conference2.style.display = 'block'});
+
+conference.addEventListener('mouseleave', () => 
+{conference2.style.display = 'none'});
+
+conference2.addEventListener('mouseleave', () => 
 {conference2.style.display = 'none'});
 // Conference end
 
 // Videos Start
 const videos = document.getElementById("videos");
 const videos2 = document.getElementsByClassName("videos")[0];
-videos.addEventListener('mouseover', () => 
+videos.addEventListener('mousemove', () => 
 {videos2.style.display = 'block'});
 
-videos.addEventListener('mouseout', () => 
+videos2.addEventListener('mousemove', () => 
+{videos2.style.display = 'block'});
+
+videos.addEventListener('mouseleave', () => 
+{videos2.style.display = 'none'});
+
+videos2.addEventListener('mouseleave', () => 
 {videos2.style.display = 'none'});
 // Videos End
 
 // PM Speak Series Start
 const pm = document.getElementById("pm");
 const pm2 = document.getElementsByClassName("pm")[0];
-pm.addEventListener('mouseover', () => 
+pm.addEventListener('mousemove', () => 
 {pm2.style.display = 'block'});
 
-pm.addEventListener('mouseout', () => 
+pm2.addEventListener('mousemove', () => 
+{pm2.style.display = 'block'});
+
+pm.addEventListener('mouseleave', () => 
+{pm2.style.display = 'none'});
+
+pm2.addEventListener('mouseleave', () => 
 {pm2.style.display = 'none'});
 // PM Speak Series End
 
 // Blogs Start
 const blogs = document.getElementById("blogs");
 const blogs2 = document.getElementsByClassName("blogs")[0];
-blogs.addEventListener('mouseover', () => 
+blogs.addEventListener('mousemove', () => 
 {blogs2.style.display = 'block'});
 
-blogs.addEventListener('mouseout', () => 
+blogs2.addEventListener('mousemove', () => 
+{blogs2.style.display = 'block'});
+
+blogs.addEventListener('mouseleave', () => 
+{blogs2.style.display = 'none'});
+
+blogs2.addEventListener('mouseleave', () => 
 {blogs2.style.display = 'none'});
 // Blogs End
 
 // Podcasts Start
 const podcasts = document.getElementById("podcasts");
 const podcasts2 = document.getElementsByClassName("podcasts")[0];
-podcasts.addEventListener('mouseover', () => 
+podcasts.addEventListener('mousemove', () => 
 {podcasts2.style.display = 'block'});
 
-podcasts.addEventListener('mouseout', () => 
+podcasts2.addEventListener('mousemove', () => 
+{podcasts2.style.display = 'block'});
+
+podcasts.addEventListener('mouseleave', () => 
+{podcasts2.style.display = 'none'});
+
+podcasts2.addEventListener('mouseleave', () => 
 {podcasts2.style.display = 'none'});
 // Podcasts End
 
 // Be A Speaker Start
 const speaker = document.getElementById("speaker");
 const speaker2 = document.getElementsByClassName("speaker")[0];
-speaker.addEventListener('mouseover', () => 
+speaker.addEventListener('mousemove', () => 
 {speaker2.style.display = 'block'});
 
-speaker.addEventListener('mouseout', () => 
+speaker2.addEventListener('mousemove', () => 
+{speaker2.style.display = 'block'});
+
+speaker.addEventListener('mouseleave', () => 
+{speaker2.style.display = 'none'});
+
+speaker2.addEventListener('mouseleave', () => 
 {speaker2.style.display = 'none'});
 // Be A Speaker End
 
 // Be A Volunteer Start
 const volunteer = document.getElementById("volunteer");
 const volunteer2 = document.getElementsByClassName("volunteer")[0];
-volunteer.addEventListener('mouseover', () => 
+volunteer.addEventListener('mousemove', () => 
 {volunteer2.style.display = 'block'});
 
-volunteer.addEventListener('mouseout', () => 
+volunteer2.addEventListener('mousemove', () => 
+{volunteer2.style.display = 'block'});
+
+volunteer.addEventListener('mouseleave', () => 
+{volunteer2.style.display = 'none'});
+
+volunteer2.addEventListener('mouseleave', () => 
 {volunteer2.style.display = 'none'});
 // Be A Volunteer End
 
 // About Us Start 
 const about = document.getElementById("about");
 const about2 = document.getElementsByClassName("about")[0];
-about.addEventListener('mouseover', () => 
+about.addEventListener('mousemove', () => 
 {about2.style.display = 'block'});
 
-about.addEventListener('mouseout', () => 
+about2.addEventListener('mousemove', () => 
+{about2.style.display = 'block'});
+
+about.addEventListener('mouseleave', () => 
+{about2.style.display = 'none'});
+
+about2.addEventListener('mouseleave', () => 
 {about2.style.display = 'none'});
 // About Us End
